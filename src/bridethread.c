7cc608cadca57884133d6a75fc4734ce745af4ca
TristanWellman <wellmanboy06@gmail.com>
2025-10-22 17:53:07 -0400

    Fix: Old Bridethread issues

--- src/bridethread.c
+++ src/bridethread.c
@@ -1,23 +1,37 @@
 /*Copyright (c) 2024 Tristan Wellman*/
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <OE/bridethread.h>
 
 struct threadData ThreadData;
 
 #ifdef _WIN32
 DWORD WINAPI ThreadWrapper(LPVOID param) {
-    void (*func)(void) = (void (*)(void))param;
-    func();
+	BRIDEFUNC *funcP = (BRIDEFUNC *)param;
+	BRIDEFUNC func = *funcP;
+	free(funcP);
+	func();
     return 0;
+}
+#else
+void *ThreadWrapper(void *param) {
+	BRIDEFUNC *funcP = (BRIDEFUNC *)param;
+	BRIDEFUNC func = *funcP;
+	free(funcP);
+	func();
+	return NULL;
 }
 #endif
 
 void startThread(char *ID, BRIDEFUNC function) {
+	BRIDEFUNC *arg = calloc(1, sizeof(BRIDEFUNC));
+	*arg = function;
     if (ThreadData.threads[MAXSIMOTHREADS - 1] != 0) {
 #ifdef _WIN32
         ThreadData.threads[0] = CreateThread(NULL, 0, ThreadWrapper, (LPVOID)function, 0, NULL);
 #else
-        pthread_create(&ThreadData.threads[0], NULL, (void *)function, NULL);
+        pthread_create(&ThreadData.threads[0], NULL, ThreadWrapper, (void *)arg);
 #endif
         ThreadData.IDs[0] = ID;
         return;
@@ -29,7 +43,7 @@ void startThread(char *ID, BRIDEFUNC function) {
 #ifdef _WIN32
             ThreadData.threads[i] = CreateThread(NULL, 0, ThreadWrapper, (LPVOID)function, 0, NULL);
 #else
-            pthread_create(&ThreadData.threads[i], NULL, (void *)function, NULL);
+            pthread_create(&ThreadData.threads[i], NULL, ThreadWrapper, (void *)arg);
 #endif
         }
         if (ThreadData.IDs[i] == NULL || ThreadData.threads[i] == 0) {
@@ -37,7 +51,7 @@ void startThread(char *ID, BRIDEFUNC function) {
 #ifdef _WIN32
             ThreadData.threads[i] = CreateThread(NULL, 0, ThreadWrapper, (LPVOID)function, 0, NULL);
 #else
-            pthread_create(&ThreadData.threads[i], NULL, (void *)function, NULL);
+            pthread_create(&ThreadData.threads[i], NULL, ThreadWrapper, (void *)arg);
 #endif
             break;
         }
